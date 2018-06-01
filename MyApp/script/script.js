18e46036b7e7a31e95799dd634db09cbd971b5f3
iguchyu <iguchyu@iguchyuMacBookPro.local>
2018-06-01 14:56:25 +0900

    change index.html, script.js

--- MyApp/script/script.js
+++ MyApp/script/script.js
@@ -1,5 +1,3 @@
-var sparql = ""
-
 $(document).ready(function(){
 	//Selectタグの変化
 	$("#query").change(function() {	
@@ -7,12 +5,10 @@ $(document).ready(function(){
 		if(this.value === "Bar") {
             var name = "google.visualization.BarChart";
 			$("div#sgvzl_example_table").attr("data-sgvizler-chart", name);
-			$("chartName").text($("#query option:selected").text());
 			sgvizler.containerDrawAll();
 		} else if(this.value === "Table") {
             var name = "google.visualization.Table";
 			$("div#sgvzl_example_table").attr("data-sgvizler-chart", name);
-			$("chartName").text($("#query option:selected").text());
 			sgvizler.containerDrawAll();
 		}
 	});
