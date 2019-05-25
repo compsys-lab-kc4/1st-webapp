d54d2833c3806f8cb2788ff111884e7a5112df0c
tsui49 <kyokyokyon4@gmail.com>
2019-05-25 23:56:28 +0900

    微修正

--- MyChartApp/input-jquery.js
+++ MyChartApp/input-jquery.js
@@ -1,23 +1,18 @@
-(function($){
-    function formSetDay(){
-        var lastday = formSetLastDay($('.chmon').val());
-        var option = '';
-        for (var i = 1; i <= lastday; i++) {
-            if (i === $('chday').val()) {
-                option += '<option value="' + i + '" selected="selected">' + i + '</option>\n';
-            } else {
-                option += '<option value="' + i + '">' + i + '</option>\n';
-            }
+function formSetDay() {
+    var lastday = formSetLastDay($('.chmon').val());
+    var option = '';
+    for (var i = 1; i <= lastday; i++) {
+        if (i === $('.chday').val()) {
+            option += '<option value="' + i + '" selected="selected">' + i + '</option>\n';
+        } else {
+            option += '<option value="' + i + '">' + i + '</option>\n';
         }
-        $('chday').html(option);
     }
+    $('.chday').html(option);
+}
 
-    function formSetLastDay(month){
-        var lastday = new Array('', 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-        return lastday[month];
-    }
+function formSetLastDay(month) {
+    var lastday = new Array(0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+    return lastday[month];
+}
 
-    $(chmon).change(function(){
-        formSetDay();
-    });
-});
